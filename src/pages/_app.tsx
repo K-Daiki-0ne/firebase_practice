5a39a3beddd1f5dfb6b1642810243bb947502bb0
_ 0ne <kouguchi3456@yahoo.co.jp>
2022-05-08 15:51:23 +0900

    setup: material-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../styles/theme';
+import createEmotionCache from '../createEmotionCache';
 import { store } from '../store/store';
 import { Provider } from 'react-redux';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 export default MyApp
