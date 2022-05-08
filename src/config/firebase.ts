42a479df39265626bee440043fad46d26b8faadf
_ 0ne <kouguchi3456@yahoo.co.jp>
2022-05-08 15:04:03 +0900

    create: firebase config file

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from  'firebase/auth';
+import { getStorage } from "firebase/storage";
+
+const config = {
+  apiKey:             process.env.NEXT_PUBLIC_APIKEY,
+  authDomain:         process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId:          process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket:      process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId:  process.env.NEXT_PUBLIC_MESSAGEINGSENDERID,
+  appId:              process.env.NEXT_PUBLIC_APPID,
+  measurementId:      process.env.NEXT_PUBLIC_MEASUREMENTID
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+export const db       = getFirestore(firebaseApp);
+export const auth     = getAuth();
+export const storage  = getStorage();
+export const provider = new GoogleAuthProvider();
