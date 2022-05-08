c6792121028d24e50d5a1657de6d38dfc2e9700d
_ 0ne <kouguchi3456@yahoo.co.jp>
2022-05-08 16:06:40 +0900

    create: auth function in home page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUser, login, logout } from '../store/userSlice';
+import { auth } from '../config/firebase';
 
+const Home: NextPage = (): JSX.Element => {
+  const user     = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-const Home: NextPage = () => {
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    }
+  }, [dispatch]);
+
   return (
     <div>Hello</div>
   )
