0f70fea58de8d2486b687efa58709ca99e8f99b5
_ 0ne <kouguchi3456@yahoo.co.jp>
2022-05-08 16:22:16 +0900

    modified: design code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,14 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectUser, login, logout } from '../store/userSlice';
 import { auth } from '../config/firebase';
+import { Feed } from '../components/Feed/Feed';
+import { Auth } from '../components/Auth/Auth';
+import useStyle from '../styles/style';
 
 const Home: NextPage = (): JSX.Element => {
   const user     = useSelector(selectUser);
   const dispatch = useDispatch();
+  const classes = useStyle();
 
   useEffect(() => {
     const unSub = auth.onAuthStateChanged((authUser) => {
@@ -28,7 +32,17 @@ const Home: NextPage = (): JSX.Element => {
   }, [dispatch]);
 
   return (
-    <div>Hello</div>
+    <>
+    {
+      user.uid ? (
+        <div className={classes.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )
+    }
+    </>
   )
 }
 
